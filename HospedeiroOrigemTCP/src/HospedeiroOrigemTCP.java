9f38362c1a73cee8c495aed893b139d5405c2212
Nathan Gonçalves <nathan@liberaai.com>
2023-09-20 16:51:24 -0300

    Primeira Implementacao Sockets Cliente Java

--- HospedeiroOrigemTCP/src/HospedeiroOrigemTCP.java
+++ HospedeiroOrigemTCP/src/HospedeiroOrigemTCP.java
@@ -1,5 +1,23 @@
+import java.net.*;
+import java.io.*;
+
 public class HospedeiroOrigemTCP {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Insira o Ip do hospedeiro de destino:");
+        BufferedReader ipDestinoReader = new BufferedReader(new InputStreamReader(System.in));
+        InetAddress ipDestino = InetAddress.getByName(ipDestinoReader.readLine());
+
+        System.out.println("Insira a Porta do hospedeiro de destino:");
+        BufferedReader portaDestinoReader = new BufferedReader(new InputStreamReader(System.in));
+        int portaDestino = Integer.parseInt(portaDestinoReader.readLine());
+
+        System.out.println(ipDestino);
+
+        Socket clienteSocket = new Socket(ipDestino, portaDestino);
+        BufferedOutputStream saida = new BufferedOutputStream(clienteSocket.getOutputStream());
+        String teste = "Hello World!";
+        saida.flush();
+        saida.write(teste.getBytes(), 0, teste.getBytes().length);
+        saida.close();
     }
 }
