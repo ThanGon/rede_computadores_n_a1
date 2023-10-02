61a54538a27d411ec983e7a2bab6563f8b9b84cb
ThanGon <nate.goncalves@gmail.com>
2023-10-02 20:38:36 -0300

    Melhorias

--- HospedeiroOrigemTCP/HospedeiroOrigemTCP.java
+++ HospedeiroOrigemTCP/HospedeiroOrigemTCP.java
@@ -8,6 +8,7 @@ import java.io.*;
 
 public class HospedeiroOrigemTCP {
     public static void main(String[] args) throws Exception {
+        // CONFIGURAÇÃO DE CONEXÃO DO SERVIDOR
         System.out.println("Insira o Ip do hospedeiro de destino:");
         BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
         InetAddress ipDestino = InetAddress.getByName(consoleReader.readLine());
@@ -23,23 +24,37 @@ public class HospedeiroOrigemTCP {
         consoleReader.close();
 
         File arquivo = new File(caminhoArquivo);
-        
+
+        // LENDO ARQUIVO I/O DO DISCO PARA BUFFER DE MEMÓRIA
         FileInputStream fis = new FileInputStream(arquivo);
         BufferedInputStream bis = new BufferedInputStream(fis);
         byte[] arquivoBytes = new byte[(int) arquivo.length()];
         bis.read(arquivoBytes);
         bis.close();
 
+        // TENTA CONECTAR AO DESTINO
         Socket clienteSocket = new Socket(ipDestino, portaDestino);
 
+        float startTime = System.nanoTime();
         ObjectOutputStream saida = new ObjectOutputStream(clienteSocket.getOutputStream());
-        
+
+        // ENVIA STREAM DE ARQUIVO
         saida.writeObject(new Mensagem(arquivo.getName(), arquivoBytes));
-        // saida.writeUTF(arquivoNome);
-        // saida.write(arquivoBytes);
 
         saida.flush();
         saida.close();
         clienteSocket.close();
+
+        double endTime = System.nanoTime();
+
+        // CONVERTE DURAÇÃO DE NANO SEGUNDOS PARA SEGUNDOS
+        double duracao = (endTime - startTime) / 1e9;
+
+        double tamanhoArquivoEmBits = arquivo.length() * 8;
+
+        // DIVIDE POR 1e6 PARA CONVERTER PARA MEGABITS
+        double taxaDeTransferenciaMbps = tamanhoArquivoEmBits / duracao / 1e6f;
+
+        System.out.println("Taxa de transferência: " + taxaDeTransferenciaMbps + " Mbps");
     }
 }
